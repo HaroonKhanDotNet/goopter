4aa82ba53a55d1217ef26ec4b30025b9d27becc3
savindarya <savindarya@gmail.com>
2023-02-13 13:23:15 -0800

    test commit and push

---
+++ goopter_products.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from time import sleep
+
+import goopter_locators
+
+service_obj = Service('chromedriver.exe')
+driver = webdriver.Chrome(service=service_obj)
+
+
+def set_up():
+    driver.maximize_window()
+    driver.implicitly_wait(30)
+
+    driver.get(goopter_locators.goopter_url)
+    sleep(2)
+    driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div/div/div[2]/button[2]/span').click()
+    sleep(2)
+
+    if driver.title == 'Goopter Business Portal':
+        print('We are seeing the title---"Goopter Business Portal, Congrats!"')
+    else:
+        print("Please check your code")
+        driver.close()
+        driver.quit()
+
+
+def log_in():
+    driver.find_element(By.NAME, 'username').send_keys(goopter_locators.username)
+    driver.find_element(By.NAME, 'password').send_keys(goopter_locators.password)
+    sleep(5)
+
+
+set_up()
+log_in()
