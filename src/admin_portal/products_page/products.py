992a42b7af873234a2f188c04b05c6243e26b963
zaina-coder <zeinab.spears22@gmail.com>
2023-02-15 12:47:50 -0800

    Zainab-dev adding actions Chains

--- src/admin_portal/products_page/products.py
+++ src/admin_portal/products_page/products.py
@@ -1,6 +1,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 def products_main(chromeBrowser):
 
@@ -12,3 +13,9 @@ def products_searchbox_type(chromeBrowser):
 
     searchbox = chromeBrowser.find_element(By.XPATH, '/html/body/div[1]/div/section/section/main/div[2]/div/div[1]/div[1]/div[1]/span/input')
     searchbox.send_keys('bubble')
+
+
+    def ant_btn_popup(chromeBrowser):
+         dots_popup_menu = chromeBrowser.find_element(By.XPATH, '//*[@id="products-table-container"]/div/div/div[1]/div[2]/div[3]/div[2]/div/div/div[1]/div[10]/div/div/button[2]')
+         #pointer_actions mouse = new pointer_actions(chromeBrowser) 
+         ActionChains(chromeBrowser).move_to_element(dots_popup_menu).perform()
