7b8d09d16d319e86d2196a472709ede124b647ed
amir.daryoush <amir.daryoush@gmail.com>
2023-02-15 12:48:44 -0800

    change login.py cordentials

--- src/admin_portal/login.py
+++ src/admin_portal/login.py
@@ -8,10 +8,10 @@ def admin_login(chromeBrowser):
     chromeBrowser.get('https://admin-dev.goopter.com/')
 
     userName_txt = chromeBrowser.find_element(By.NAME, 'username')
-    userName_txt.send_keys('haroondev')
+    userName_txt.send_keys('amirdev')
 
     password_txt = chromeBrowser.find_element(By.NAME, 'password')
-    password_txt.send_keys('Hk20230130')
+    password_txt.send_keys('20230130')
 
     login_btn = chromeBrowser.find_element(By.XPATH, '/html/body/div/div/div/div[1]/form/div/div[3]/div/div/div/button')
     login_btn.click()
