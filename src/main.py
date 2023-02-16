419668c6d98c8a263274a087c55ae8691fc75b73
DanielYoo <mhy930607@gmail.com>
2023-02-16 15:24:50 -0800

    publishmanager

--- src/main.py
+++ src/main.py
@@ -1,27 +1,48 @@
-
+import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from admin_portal import login
-from admin_portal.products_page import products
-from admin_portal.categories_page import categories
+from admin_portal import publishmanager_page
+# from admin_portal.products_page import products
+# from admin_portal.categories_page import categories
+from admin_portal.publishmanager_page import publishmanager
+
 
 def goopter_main():
 
-    chromeBrowser = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--disable-notifications")
+
+    chromeBrowser = webdriver.Chrome(options=options)
     chromeBrowser.maximize_window()
 
     # login automation
     login.admin_login(chromeBrowser)
-    input()
+    time.sleep(3)
+
+    # publish manager automation
+    publishmanager.publishmanager_main(chromeBrowser)
+    time.sleep(2)
+
+    #publishmanager.publishmanager_republish(chromeBrowser)
+    #time.sleep(2)
+    
+    publishmanager.publishmanager_unpublish(chromeBrowser)
+    time.sleep(2)
+    
+    publishmanager.publishmanager_sync(chromeBrowser)
+    time.sleep(5)
 
     # products automation
-    products.products_main(chromeBrowser)
-    input()
+    # products.products_main(chromeBrowser)
+    # input()
 
     # products searchbox type
-    products.products_searchbox_type(chromeBrowser)
-    input()
+    # products.products_searchbox_type(chromeBrowser)
+    # input()
 
+    input()
     chromeBrowser.quit()
 
 
