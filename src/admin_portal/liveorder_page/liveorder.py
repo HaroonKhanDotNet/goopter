4fb1e8a005f95b70376fb8c1055bbe4ffd0f6766
Alexandra Sokolova <alesok2905@gmail.com>
2023-02-16 10:28:09 -0800

    Alexa liveorder Feb 16

--- src/admin_portal/liveorder_page/liveorder.py
+++ src/admin_portal/liveorder_page/liveorder.py
@@ -10,6 +10,8 @@ def add_order(chromeBrowser):
     
     
     
-    add_element = chromeBrowser.find_element(By.XPATH, '//path[contains(.,"M19 13h-6v6h-2v-6H5v-2H6V5h2v6h6v2z")]').click()
+    #add_element = chromeBrowser.find_element(By.XPATH, '//path[contains(.,"M19 13h-6v6h-2v-6H5v-2H6V5h2v6h6v2z")]').click()
+    
+    add_element = chromeBrowser.find_element(By.XPATH, "//div[@class='order-list-title-end-section']//*[name()='svg']").click()
  
-    add_element.click()   
+    #add_element.click()   
