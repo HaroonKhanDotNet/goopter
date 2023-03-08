1a5ddd6f113d43664f71716a68e50b1a8f34a6b6
Haroon Khan <info@haroonkhan.net>
2023-03-08 11:37:35 -0800

    main: diff on sel.py & del sel.py on project root

--- src/sel.py
+++ src/sel.py
@@ -2,51 +2,39 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 import logging
-
-logging.basicConfig(format='%(levelname)s - %(asctime)s - %(message)s', 
-                    datefmt='%m/%d/%Y %I:%M:%S %p',
-                    filename='logs/login_page.log', 
-                    encoding='utf-8',
-                    level = logging.INFO)
-
-
-
-chromeBrowser = webdriver.Chrome()
-logging.info("Maximizing: Chrome Browser Window...")
-chromeBrowser.maximize_window()
-logging.info("Maximized: Chrome Browser Window...")
-logging.info("Loading: https://admin-dev.goopter.com/")
-
-#* this will try to access 
+logging.basicConfig (filename='logs/login_page.log',
+                     encoding='utf-8', level=logging.INFO, format='%(levelname)s %(asctime)s - %(message)s',  datefmt='%m/%d/%Y %I:%M:%S %p')
+firefox = webdriver.Firefox()
+logging.info("Maximizing: Firefox Bowser Window ...")
+firefox.maximize_window()
+logging.info("Maximized: Firefox Bowser Window ...")
+logging.info("Loading:https://admin-dev.goopter.com/")
+# * this will try to access the login webpage three time
 pingCounter = 0
-while pingCounter < 2: 
-    try:
-        chromeBrowser.get('https://admin-dev.goopterdfg.com/')
-    except WebDriverException as ex:
-        logging.critical(f'PingCounter={pingCounter} {ex.msg}')
-        pingCounter = pingCounter + 1
- 
+while pingCounter < 3: 
+    try: firefox.get('https://admin-dev.goopterdfg.com/')
+    # todo: we need to move all the page testing code here 
+    break 
+except WebDriverException as ex: 
+    logging.error(f'pingCounter={pingCounter} {ex.msg}') 
+    pingCounter = pingCounter + 1 
+    if pingCounter == 3: 
+        logging.critical('Login Page is inaccessible')
+
 logging.info("Loaded: https://admin-dev.goopter.com/")
-logging.warning("Try: Entering ...")
-
-try:
-    logging.warning("Assert: Comparing the Page Title")
-    assert chromeBrowser.title == 'Goopter Business Porta'
-
-except AssertionError:
-    logging.error("Assert: Fail to match title...")
+logging.warning('Try: Entering ...')
+try: 
+    logging.warning('Assert: Comparing the Page Title') 
+    assert firefox.title == 'Goopter Business Porta'
+except AssertionError: 
+    logging.error('Asser: Fail to match Title ...') 
     print("Title doesn't match")
 
 try: 
-    id = chromeBrowser.find_element(By.ID, 'anyID')
-
-except NoSuchElementException:     
-    logging.error("chromeBrowser.find_element(By.ID, 'anyID')")
-
-logging.info('Try: Passed')
-
-f = open('pg.txt', 'w')
-f.write(chromeBrowser.page_source.replace('\n', ''))
-logging.info("Closing Browser")
-chromeBrowser.quit
-logging.info("Closed Browser")
+    id = firefox.find_element(By.ID, 'anyID')
+except NoSuchElementException: 
+    logging.error("firefox.find_element(By.ID, 'anyID')")
+    logging.info('Try: Passed')
+    f = open('pg.txt', 'w')
+    f.write( firefox.page_source.replace('\n', '') )
+    f.close()logging.info('Closing: Browser')
